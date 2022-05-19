a14708c86ce273ac40c5deb35c3a59ba6bb9b0a9
Nikita Radzisheuski <nickitque.39@gmail.com>
2022-05-19 22:49:15 +0300

    Added meta tag and h1

--- First_website/myscript.js
+++ First_website/myscript.js
@@ -1,12 +1,5 @@
 
     // Это текстовый комментарий
-    alert("Hello World!!!");   // Этот код выполнится.
+    // alert("Hello World!!!");   // Этот код выполнится.
     // alert("Hello World"); А этот нет
     // Он закомментирован
-
-    function testComment(a, b) {
-        var x;
-        x = a * b;
-
-        return x;
-    }
